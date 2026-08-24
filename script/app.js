479843ade23c56b0728f7d350ee6f1b60282a73c
AmirHosein <amirbolori267@gmail.com>
2026-08-24 21:26:58 +0330

    style: add header and hero styling

--- script/app.js
+++ script/app.js
@@ -1,1 +1,14 @@
 "use strict";
+const infos = document.querySelectorAll(".info");
+infos.forEach((info) => {
+  const icon = info.querySelector(".info-icon");
+  const box = info.querySelector(".info-box");
+
+  icon.addEventListener("click", () => {
+    infos.forEach((item) => {
+      item.querySelector(".info-box").classList.remove("info-box--active");
+    });
+
+    box.classList.add("info-box--active");
+  });
+});
