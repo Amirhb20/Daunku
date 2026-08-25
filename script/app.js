59a05f863a631463540ba4d81ec981f3f2f883c9
AmirHosein <amirbolori267@gmail.com>
2026-08-25 21:33:44 +0330

    style: make header and hero responsive

--- script/app.js
+++ script/app.js
@@ -1,5 +1,11 @@
 "use strict";
+const navToggleIcon = document.querySelector(".nav-toggle__icon ");
+const list = document.querySelector(".list");
 const infos = document.querySelectorAll(".info");
+navToggleIcon.addEventListener("click", function () {
+  navToggleIcon.classList.toggle("nav-toggle__icon--open");
+  list.classList.toggle("list--show");
+});
 infos.forEach((info) => {
   const icon = info.querySelector(".info-icon");
   const box = info.querySelector(".info-box");
