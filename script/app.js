979b57bbd98c9ee58f13cfb78c409eb6f959dffc
AmirHosein <amirbolori267@gmail.com>
2026-08-26 21:17:06 +0330

    style: add reference section styling

--- script/app.js
+++ script/app.js
@@ -2,6 +2,7 @@
 const navToggleIcon = document.querySelector(".nav-toggle__icon ");
 const list = document.querySelector(".list");
 const infos = document.querySelectorAll(".info");
+const images = document.querySelectorAll(".reference__img");
 navToggleIcon.addEventListener("click", function () {
   navToggleIcon.classList.toggle("nav-toggle__icon--open");
   list.classList.toggle("list--show");
@@ -18,3 +19,30 @@ infos.forEach((info) => {
     box.classList.add("info-box--active");
   });
 });
+// فعال بودن آیتم اول
+images[0].classList.add("active");
+
+images.forEach((img) => {
+  img.addEventListener("click", () => {
+    images.forEach((item) => item.classList.remove("active"));
+    img.classList.add("active");
+  });
+});
+
+const aboutSection = document.querySelector(".reference-photo");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        aboutSection.classList.add("show");
+        observer.unobserve(aboutSection); // فقط یک بار اجرا شود
+      }
+    });
+  },
+  {
+    threshold: 0.5, // 30٪ بخش دیده شود
+  },
+);
+
+observer.observe(aboutSection);
